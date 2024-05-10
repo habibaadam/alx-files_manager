f0f77f9cf1d3fc3cd3aea11c28d389d83d9697c5
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-05-10 15:41:57 +0000

    0. Redis utils

--- utils/redis.js
+++ utils/redis.js
@@ -2,30 +2,34 @@ const redis = require('redis');
 const { promisify } = require('util');
 
 class RedisClient {
-    constructor() {
-        this.client = redis.createClient();
-        this.client.on('error', (error) => {
-            console.error(`Redis client not connected to the server: ${error.message}`);
-        });
-    }
-    
+  constructor() {
+    this.client = redis.createClient();
+    this.connected = true;
+    this.client.on('error', (error) => {
+      console.error(`Redis client not connected to the server: ${error.message}`);
+      this.connected = false;
+    });
+  }
 
-    isAlive() {
-        return this.client.connected;
-    }
-    
-    async get(key) {
-        const getAsync = promisify(this.client.get).bind(this.client);
-        const value = await getAsync(key);
-        return value;
-    }
-    
-    async set(key, value, duration) {
-        this.client.set(key, value);
-        this.client.expire(key, duration);
-    }
-    
-    async del(key) {
-        this.client.del(key);
-    }
+  isAlive() {
+    return this.connected;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    const value = await getAsync(key);
+    return value;
+  }
+
+  async set(key, value, duration) {
+    this.client.set(key, value);
+    this.client.expire(key, duration);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
 }
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
