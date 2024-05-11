b8f44ffee5dcef8732265b0d53b91ce1bcc65515
memoemukoe <122824378+mukoe2020@users.noreply.github.com>
2024-05-11 12:31:57 +0200

    create user

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const appController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
 
 const router = express.Router();
 router.get('/status', appController.getStatus); // definition of getStatus
-router.get('/stats', appController.getStats); // defintion of getStats
-
+router.get('/stats', appController.getStats); // defintion of getStatus
+router.post('/users', UsersController.postNew); // definition of postNew
 module.exports = router;
