ad7e633ca81b17d52d068810c45f1caf5f150a24
memoemukoe <122824378+mukoe2020@users.noreply.github.com>
2024-05-11 12:51:17 +0200

    new code with hashed password

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,3 +1,4 @@
+const sha1 = require('sha1');
 const dbClient = require('../utils/db');
 
 class UsersController {
@@ -5,24 +6,21 @@ class UsersController {
     const { email, password } = req.body;
     if (!email) {
       res.status(400).json({ error: 'Missing email' });
-      res.end();
       return;
     }
     if (!password) {
       res.status(400).json({ error: 'Missing password' });
-      res.end();
       return;
     }
     const userExist = await dbClient.userExist(email);
     if (userExist) {
       res.status(400).json({ error: 'Already exist' });
-      res.end();
       return;
     }
-    const user = await dbClient.createUser(email, password);
+    const hashedPassword = sha1(password); // Hash the password using SHA1
+    const user = await dbClient.createUser(email, hashedPassword); // Store the hashed password in the database
     const id = `${user.insertedId}`;
     res.status(201).json({ id, email });
-    res.end();
   }
 }
 
